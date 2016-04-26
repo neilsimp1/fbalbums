54e5edf2cc10deb27b1437dd45267362b56179ac
neilsimp1 <neilsimp1@gmail.com>
2016-04-26 11:29:54 -0400

    single album endpoint, untested FB api url

--- src/app.js
+++ src/app.js
@@ -12,8 +12,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 let facebook = new Facebook(config);
 
-app.get('/:id/albums', function(req, res){
-    let url = 'http://graph.facebook.com/' + req.params.id
+app.get('/:userID/albums', function(req, res){
+    let url = 'http://graph.facebook.com/' + req.params.userID
                 + '?fields=albums.fields(id,name,created_time,photos.fields(id,name,picture,source,created_time).limit(5000))';
     facebook.api(url, function(err, data){
         let albums = [];
@@ -34,6 +34,33 @@ app.get('/:id/albums', function(req, res){
         }
         
         let json = Album.arrayToString(albums);
+        
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        //console.log(json);
+        res.end(json);
+    });
+});
+
+app.get('/:userID/:albumID', function(req, res){
+    let url = 'http://graph.facebook.com/' + req.params.userID + '/' + req.params.albumID
+                + '?fields=id,name,created_time,photos.fields(id,name,picture,source,created_time).limit(5000))';//untested url
+    facebook.api(url, function(err, data){
+        let album;
+        if(err){
+            console.error(err);
+            res.sendStatus(502);
+            res.end();
+        }
+        else{
+            album = new Album(data.id, data.name, data.created_time);
+			data.photos.data.forEach(function(_photo){
+				album.addPhoto(new Photo(_photo.id, _photo.name, _photo.picture
+								, _photo.source, _photo.created_time));
+			});
+        }
+        
+        let json = album.toString();
         
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.writeHead(200, {'Content-Type': 'application/json'});
